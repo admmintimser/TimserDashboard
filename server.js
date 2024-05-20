fb8d51bed9db3c5ab06bbcd9d6989f3eb2c1409e
admmintimser <adminti@timsergroup.com>
2024-05-20 11:16:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
