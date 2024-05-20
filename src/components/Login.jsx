6b1594c0756d5bfc74b6b6fe268d34fdb74ee7c2
admmintimser <adminti@timsergroup.com>
2024-05-20 14:58:35 -0600

    Update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     try {
       const response = await axios.post(
         "https://webapitimser.azurewebsites.net/api/v1/user/login",
-        { email, password },
+        { email, password, role: "Admin" },
         {
           withCredentials: true,
           headers: { "Content-Type": "application/json" },
