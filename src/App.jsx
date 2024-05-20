970dcf233c46c1f331406d3f6ffaec24a767b7f5
admmintimser <adminti@timsergroup.com>
2024-05-20 13:19:33 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ import AddNewDoctor from "./components/AddNewDoctor";
 import Messages from "./components/Messages";
 import Doctors from "./components/Doctors";
 import Flebos from "./components/Flebos";
-import { Context } from "./main";
-import axios from "axios";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "./components/Sidebar";
 import AddNewAdmin from "./components/AddNewAdmin";
 import AddNewFleb from "./components/AddNewFleb";
@@ -18,29 +14,41 @@ import Elisas from "./components/Elisas";
 import WesternBlot from "./components/WesternBlot";
 import Informe from './components/Informe';
 import "./App.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./main";
 
 const App = () => {
-  const { isAuthenticated, setIsAuthenticated, admin, setAdmin } =
-    useContext(Context);
+  const { isAuthenticated, setIsAuthenticated, admin, setAdmin, authToken } = useContext(Context);
 
   useEffect(() => {
     const fetchUser = async () => {
+      if (!authToken) {
+        toast.info("No authentication token found, please login.");
+        return;  // Stop the function if there is no token
+      }
       try {
-        const response = await axios.get(
-          "https://webapitimser.azurewebsites.net//api/v1/user/admin/me",
-          {
-            withCredentials: true,
-          }
-        );
+        const response = await axios.get("https://webapitimser.azurewebsites.net/api/v1/user/admin/me", {
+          headers: {
+            Authorization: `Bearer ${authToken}`,  // Assuming authToken is stored and managed in your context
+          },
+          withCredentials: true
+        });
         setIsAuthenticated(true);
         setAdmin(response.data.user);
+        toast.success("Login Successful!");
       } catch (error) {
         setIsAuthenticated(false);
         setAdmin({});
+        toast.error(`Failed to fetch user data: ${error.response ? error.response.data.message : error.message}`);
       }
     };
-    fetchUser();
-  }, [isAuthenticated]);
+
+    if (!isAuthenticated) {
+      fetchUser();
+    }
+  }, [isAuthenticated, setIsAuthenticated, setAdmin, authToken]);  // Include authToken in the dependencies array
 
   return (
     <Router>
@@ -57,7 +65,7 @@ const App = () => {
         <Route path="/dashclient" element={<DashboardClient />} />
         <Route path="/elisas" element={<Elisas />} />
         <Route path="/westernblot" element={<WesternBlot />} />
-        <Route path="/data-for-dashboard" element={<Informe />}></Route>
+        <Route path="/data-for-dashboard" element={<Informe />} />
       </Routes>
       <ToastContainer position="top-center" />
     </Router>
