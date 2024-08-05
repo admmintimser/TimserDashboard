6036404a5226a75a0ba60e264f6cd33a426a7841
admmintimser <adminti@timsergroup.com>
2024-08-05 11:07:32 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,33 @@ import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-export const Context = createContext({ 
-  isAuthenticated: false, 
-  authToken: null 
-});
+export const Context = createContext({ isAuthenticated: false });
 
 const AppWrapper = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [authToken, setAuthToken] = useState(null);  
-  const [admin, setAdmin] = useState({});
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [admin, setAdmin] = useState({});
+    const [userRole, setUserRole] = useState(""); // Nuevo estado para el rol del usuario
 
-  return (
-    <Context.Provider
-      value={{ isAuthenticated, setIsAuthenticated, authToken, setAuthToken, admin, setAdmin }}
-    >
-      <App />
-    </Context.Provider>
-  );
+    return (
+        <Context.Provider
+            value={{
+                isAuthenticated,
+                setIsAuthenticated,
+                admin,
+                setAdmin,
+                userRole,
+                setUserRole // Añadir setter para el rol del usuario
+            }}
+        >
+            <App />
+        </Context.Provider>
+    );
 };
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>
-);
+ReactDOM
+    .createRoot(document.getElementById("root"))
+    .render(
+        <React.StrictMode>
+            <AppWrapper />
+        </React.StrictMode>
+    );
